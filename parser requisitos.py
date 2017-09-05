29f0f1b57aa353a4d59b1840de15f285ab1a39ba
RodrigoNdeC <rodrigondec@gmail.com>
2017-09-05 12:16:33 -0300

    parser com layout

--- parser requisitos.py
+++ parser requisitos.py
@@ -16,6 +16,10 @@ for requisito in tmp:
     x['descricao'] = requisito['Descrição']
     requisitos.append(x)
 
-# print(requisitos)
-for requisito in requisitos:
-	print(requisito)
+# # print(requisitos)
+# for requisito in requisitos:
+layout = "Requirement {0} ( {1} ) [\n"\
+"\ttext = {2}\n"\
+"]"
+print(layout.format('Nome', 'Codigo', 'Texto'))
+
