10cb4498ecc9f6152f6df2837377f80e9d78688c
RodrigoNdeC <rodrigondec@gmail.com>
2017-09-05 12:23:10 -0300

    parser quase pronto

--- parser requisitos.py
+++ parser requisitos.py
@@ -11,13 +11,13 @@ with open("requisitos_estacionamento.csv", 'r', encoding='utf-8') as f:
 
 for requisito in tmp:
     x = dict()
-    x['codigo'] = requisito['Requisito']
-    x['nome'] = requisito['Nome']
+    x['codigo'] = requisito['Requisito'].replace('RF', '')
+    x['nome'] = requisito['Nome'].replace('\n', '')
     x['descricao'] = requisito['Descrição']
     requisitos.append(x)
 
 # # print(requisitos)
-# for requisito in requisitos:
-layout = "Requirement {0} ( {1} ) {{\n\ttext = {2}\n}}"
-print(layout.format('Nome', 'Codigo', 'Texto'))
+for requisito in requisitos:
+	layout = "Requirement {0} ( {1} ) {{\n\ttext = {2}\n}}\n"
+	print(layout.format(requisito['nome'], requisito['codigo'], requisito['descricao']))
 
