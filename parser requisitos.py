87952a1e39895bb478e99702ef97630c0b6322ef
RodrigoNdeC <rodrigondec@gmail.com>
2017-09-11 00:08:47 -0300

    parser ok

--- parser requisitos.py
+++ parser requisitos.py
@@ -12,12 +12,13 @@ with open("requisitos_estacionamento.csv", 'r', encoding='utf-8') as f:
 for requisito in tmp:
     x = dict()
     x['codigo'] = requisito['Requisito'].replace('RF', '')
-    x['nome'] = requisito['Nome'].replace('\n', '')
+    x['nome'] = requisito['Nome'].replace('\n', '').replace(' ', '')
     x['descricao'] = requisito['Descrição']
     requisitos.append(x)
 
-# # print(requisitos)
-for requisito in requisitos:
-	layout = "Requirement {0} ( {1} ) {{\n\ttext = {2}\n}}\n"
-	print(layout.format(requisito['nome'], requisito['codigo'], requisito['descricao']))
-
+# print(requisitos)
+with open("My.sysadl", "a") as myfile:
+	myfile.write("\n")
+	for requisito in requisitos:
+		layout = "Requirement {0} ( {1} ) {{\n\ttext = \"{2}\"\n}}\n"
+		myfile.write(layout.format(requisito['nome'], requisito['codigo'], requisito['descricao']))
